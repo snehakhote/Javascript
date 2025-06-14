7a4acff3b87ad7629cb3ad651aacce8973d1aca7
sneha khote <snehakhote35@gmail.com>
2025-06-14 15:00:08 +0000

    summary

---
+++ learn/functions.js
@@ -0,0 +1,26 @@
+//...................*premitives data types*........//
+//7 TYPES HOTE HAI = string ,number, boolean,null,undefined,symbol,Bigint// 
+
+//......*non primitives*...//
+// array ,objects ,function
+
+
+//const scorevalue =100.3
+//const isLoggedIn = false
+//const outsideTemp = null
+//let userEmail;//
+
+//cont id = symbol ('123')
+const anotherId = symbol('123')
+console.log(id===anotherId);
+const heros =["shaktiman" , "naagra" , "doge"];
+
+ let myobject= {
+    name: " sneha"
+    age: 22,
+
+}
+ const myFunction = function(){
+    console.log("hello world");
+ }
+ 
